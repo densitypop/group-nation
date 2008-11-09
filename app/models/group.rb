2fd756cac236660c791dfc703f2a7263a7c33a0c
faithfulgeek <joe@faithfulgeek.org>
2008-11-09 12:43:58 -0500

    Setup group model.
    Added validators, permalink, and organizer association to the group model. Tested it
    all in the console to make sure it works.

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,15 @@
 class Group < ActiveRecord::Base
 
   validates_presence_of :name
+  validates_presence_of :organizer
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
 
-  belongs_to :organizer, :class => :person
+  validates_format_of :zip, :with => /\d{5}/
+
+  belongs_to :organizer, :class_name => :person
+
+  has_permalink :name
 
 end
