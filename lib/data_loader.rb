cbd1913cd808d2241e60788da79ef83830315bb2
faithfulgeek <joe@faithfulgeek.org>
2008-12-06 19:23:14 -0500

    Data loader now uses method_missing to do loading and uses a create_<model name> method to create the object. See the spec for more detaills

--- lib/data_loader.rb
+++ lib/data_loader.rb
@@ -2,42 +2,58 @@ require 'forgery'
 
 class DataLoader
 
-  def self.load_people
+  def create_person
 
     f = InternetEnabledPersonForgery
 
-    50.times do
-      Person.create :first_name =>  f.first_name,
-                      :last_name => f.last_name,
-                      :email => f.email_address
-      f.reset
-    end
+    Person.create :first_name =>  f.first_name,
+                    :last_name => f.last_name,
+                    :email => f.email_address
+    f.reset
 
   end
 
-  def self.load_groups
+  def create_group
 
     f = GroupForgery
 
-    20.times do
-      Group.create :name => f.name,
-                    :city => f.city,
-                    :state => f.state,
-                    :zip => f.zip,
-                    :active => group_active_state,
-                    :organizer => group_organizer
+    Group.create :name => f.name,
+                  :city => f.city,
+                  :state => f.state,
+                  :zip => f.zip,
+                  :active => group_active_state,
+                  :organizer => group_organizer
+
+  end
+
+
+  def self.method_missing(method, *args)
+
+    if method.to_s =~ /^load_/
+      model_name = method.to_s.gsub(/^load_/, '').singularize
+      model_class = model_name.classify.constantize
+
+      model_class.delete_all
+
+      number_of_objects = args[0]
+
+      number_of_objects.times do
+        self.new.send "create_#{model_name}"
+      end
+
     end
 
   end
 
 private
 
-  def self.group_active_state
-    false if Group.find_all_by_active(false).length < 5
+  def group_active_state
+    return false if Group.find_all_by_active(false).length < 5
     true
   end
 
-  def self.group_organizer
+  def group_organizer
+    p = nil
     begin
       p = Person.first :order => 'rand()'
     end while Group.find_all_by_organizer_id(p.id).length > 3
--- spec/lib/data_loader_spec.rb
+++ spec/lib/data_loader_spec.rb
@@ -3,9 +3,30 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe "The data loader" do
 
   it "should load 50 random people" do
-
     Person.should_receive(:create).exactly(50).times
-    DataLoader.load_people
+    DataLoader.load_people 50
+  end
+
+  it "should load 20 random groups" do
+    Group.should_receive(:create).exactly(20).times
+    DataLoader.load_people 50
+    DataLoader.load_groups 20
+  end
+
+  describe "after loading groups" do
+
+    before(:each) do
+      DataLoader.load_people 50
+      DataLoader.load_groups 20
+    end
+
+    it "should have 5 inactive groups" do
+      Group.find_all_by_active(false).length.should == 5
+    end
+
+    it "should not have an organizer with more than 3 groups" do
+      Person.all.each { |p| p.groups.length.should_not be_greater_than(3) }
+    end
 
   end
 
